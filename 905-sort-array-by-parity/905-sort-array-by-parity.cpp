068fa6641ea439dd2e3bd3c8f726162d707f4afb
shivesh41kr <87690329+shivesh41kr@users.noreply.github.com>
2022-05-02 10:09:05 +0530

    Time: 23 ms (16.24%), Space: 16.3 MB (25.41%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int>v;
-        vector<int>v1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2!=0){
-                v1.push_back(nums[i]);
-            }
-            else{
-                v.push_back(nums[i]);
+    vector<int> sortArrayByParity(vector<int>& n) {
+        int i=0;
+        for(int j=0;j<n.size();j++){
+            if(n[j]%2==0){
+                swap(n[i],n[j]);
+                    i++;
             }
         }
-        for(int i=0;i<v1.size();i++){
-            v.push_back(v1[i]);
-        }
-        return v;
+        return n;
     }
 };
