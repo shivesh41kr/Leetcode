03bceb0f1e41f3a05661f4a53c30d42a6619ba93
S4kr_sk <87690329+shivesh41kr@users.noreply.github.com>
2022-07-06 12:37:29 +0530

    Time: 21 ms (17.74%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int helper(int n){
+        if(n==0) {
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        return helper(n-1)+helper(n-2);
+    };
+    int fib(int n) {
+      
+        return helper(n);
+    }
+};
