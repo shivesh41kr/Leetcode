5459779ed612fe2f711216ecd4cedb4494df9d42
S4kr_sk <87690329+shivesh41kr@users.noreply.github.com>
2022-07-30 12:29:51 +0530

    Time: 51 ms (40.17%), Space: 17 MB (52.62%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long int changeHelper(vector<int>S, int n){
+        int m=S.size();
+      long long int dp[m+1][n+1];
+      for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){ 
+            if(i==0){
+                dp[i][j]=0;
+            }
+           if(j==0){
+            dp[i][j]=1;
+        } 
+         }
+      }
+          for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(S[i-1]<=j){
+                    dp[i][j]=dp[i][j-S[i-1]] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+  }
+    int change(int amount, vector<int>& coins) {
+        return changeHelper(coins,amount);
+    }
+};
